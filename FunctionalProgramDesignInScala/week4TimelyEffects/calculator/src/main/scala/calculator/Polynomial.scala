840d5d12a01f8d3f13af58571c1bcd732f6b5436
David Tan <dtan@luc.edu>
2016-06-14 10:58:52 -0500

    first attempt on assignment 4 calculator. Signal as transient values using polynomial as case example

--- FunctionalProgramDesignInScala/week4TimelyEffects/calculator/src/main/scala/calculator/Polynomial.scala
+++ FunctionalProgramDesignInScala/week4TimelyEffects/calculator/src/main/scala/calculator/Polynomial.scala
@@ -1,13 +1,24 @@
 package calculator
 
 object Polynomial {
-  def computeDelta(a: Signal[Double], b: Signal[Double],
-      c: Signal[Double]): Signal[Double] = {
-    ???
+  //Δ = b² - 4ac
+  //http://www.themathpage.com/aprecalc/complete-the-square.htm
+  def computeDelta(a: Signal[Double], b: Signal[Double], c: Signal[Double]): Signal[Double] = {
+    Signal({
+      val b_sig = b()
+      (b_sig * b_sig) - 4*(a()*c())
+    })
   }
+//(-b ± √Δ) / 2a-->
+  def computeSolutions(a: Signal[Double], b: Signal[Double], c: Signal[Double], delta: Signal[Double]): Signal[Set[Double]] = {
+    Signal({
+      def findCompleteQuadratic(Δ:Double): Double = (-b()+ Δ)/ (2*a())
 
-  def computeSolutions(a: Signal[Double], b: Signal[Double],
-      c: Signal[Double], delta: Signal[Double]): Signal[Set[Double]] = {
-    ???
+      delta() match {
+        case x if x>0 => val sqrt = Math.sqrt(x);Set(findCompleteQuadratic(-sqrt),findCompleteQuadratic(sqrt))
+        case y if y<0 => Set(findCompleteQuadratic(0))
+        case _ => Set()
+      }
+    })
   }
 }
--- FunctionalProgramDesignInScala/week4TimelyEffects/calculator/src/main/scala/calculator/TweetLength.scala
+++ FunctionalProgramDesignInScala/week4TimelyEffects/calculator/src/main/scala/calculator/TweetLength.scala
@@ -4,11 +4,29 @@ object TweetLength {
   final val MaxTweetLength = 140
 
   def tweetRemainingCharsCount(tweetText: Signal[String]): Signal[Int] = {
-    ???
+    Signal({
+      MaxTweetLength - tweetLength(tweetText())
+    })
   }
 
   def colorForRemainingCharsCount(remainingCharsCount: Signal[Int]): Signal[String] = {
-    ???
+    Signal({
+      getColor(remainingCharsCount())
+    })
+  }
+/**
+   From remaining char count to "warning" color
+   For better visual feedback, we also want to display the remaining character
+   count in colors indicating how "safe" we are:
+
+   If there are 15 or more characters left, the color "green"
+   If there are between 0 and 14 characters left, included, the color "orange"
+   Otherwise (if the remaining count is negative), the color "red"
+ */
+  def getColor(remainingCharsCount:Int)= remainingCharsCount match{
+    case a if a>14 =>"green"
+    case b if b>=0 =>"orange"
+    case _  =>"red"
   }
 
   /** Computes the length of a tweet, given its text string.
@@ -24,8 +42,6 @@ object TweetLength {
      */
     if (text.isEmpty) 0
     else {
-      text.length - text.init.zip(text.tail).count(
-          (Character.isSurrogatePair _).tupled)
-    }
+      text.length - text.init.zip(text.tail).count((Character.isSurrogatePair _).tupled)}
   }
 }
